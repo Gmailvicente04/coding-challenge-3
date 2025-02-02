f129808b5210b35192d1e3b4a74c2aee2e65678b
Gmailvicente04 <vicente.oswald@gmail.com>
2025-02-01 22:45:55 -0500

    "Task 1 - Product Price Management."

--- cc_3.js
+++ cc_3.js
@@ -1,1 +1,12 @@
 //Task 1
+// Declare an array with at least five numerical values
+let prices = [9.99, 18.49, 29.99, 27.75, 49.00];
+
+// Add a new price to the array
+prices.push(25.99);
+
+// Remove the first price from the array
+prices.shift();
+
+// Display the updated prices array
+console.log(prices);
